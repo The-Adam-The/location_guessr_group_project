9adaba17b6e3a1272467c9681e6af9c279772788
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-03-12 16:44:26 +0000

    changed returns in app.js and MainContainer to get the right hierachy

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,6 +1,14 @@
+import LogInContainer from './LogInContainer';
+import GameContainer from './GameContainer';
+import ScoresContainer from './ScoresContainer';
+
 const MainContainer = () => {
     return(
-        <></>
+        <>
+            <LogInContainer/>
+            <GameContainer/>
+            <ScoresContainer/>
+        </>
     );
 };
 
