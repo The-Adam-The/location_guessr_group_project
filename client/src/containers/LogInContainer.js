1dca4eedd8c3ab82dedefe796967c50ec4712ed9
James <jamesg487@gmail.com>
2022-03-13 14:50:19 +0000

    added conditionals for pages with userName state

--- client/src/containers/LogInContainer.js
+++ client/src/containers/LogInContainer.js
@@ -1,6 +1,12 @@
-const LogInContainer = () => {
+import LogInForm from '../components/LogInForm'
+import Instructions from '../components/Instructions'
+
+const LogInContainer = ({displayGamePage, setUserName}) => {
     return(
-        <></>
+        <>
+            <LogInForm displayGamePage={displayGamePage} setUserName={setUserName}/>
+            <Instructions/>
+        </>
     );
 };
 
--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -7,18 +7,32 @@ import ScoresService from '../services/ScoresService';
 const MainContainer = () => {
 
     const [scores, setScores] = useState([]);
+    const [page, setPage] = useState('LogInPage')
+    const [userName, setUserName] = useState('')
 
     useEffect(() => {
         ScoresService.getScores()
         .then(scores => setScores(scores))
-    }, [])
+    }, [page])
+    
+    const displayLogInPage = () => {
+        setPage('LogInPage')
+    }
+
+    const displayGamePage = () => {
+        setPage('GamePage')
+    }
+
+    const displayScoresPage = () => {
+        setPage('ScoresPage')
+    }
 
     return(
-        <>
-            <LogInContainer/>
-            <GameContainer/>
-            <ScoresContainer scores={scores}/>
-        </>
+        <div>
+            {page === 'LogInPage' ? <LogInContainer displayGamePage={displayGamePage} setUserName={setUserName}/> : null}
+            {page === 'GamePage' ? <GameContainer displayScoresPage={displayScoresPage}/> : null}
+            {page === 'ScoresPage' ?<ScoresContainer scores={scores} displayLogInPage={displayLogInPage}/> : null}
+        </div>
     );
 };
 
