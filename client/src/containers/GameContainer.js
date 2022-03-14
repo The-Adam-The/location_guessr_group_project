c407b4948ec659087afcc0485eca29d8d2bd7c30
hvirfvel <carolinehvirfvel@hotmail.com>
2022-03-14 11:35:15 +0000

    changes var to let in haversineDistance calculation

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -53,13 +53,13 @@ const {isLoaded, loadError} = useLoadScript({
 
     // this function calculates the distance between two locations with lat and lng values
     const haversineDistance = (mk1, mk2) => {
-        var R = 3958.8; // Radius of the Earth in miles
-        var rlat1 = mk1.lat * (Math.PI/180); // Convert degrees to radians
-        var rlat2 = mk2.lat * (Math.PI/180); // Convert degrees to radians
-        var difflat = rlat2-rlat1; // Radian difference (latitudes)
-        var difflon = (mk2.lng-mk1.lng) * (Math.PI/180); // Radian difference (longitudes)
+        let R = 3958.8; // Radius of the Earth in miles
+        let rlat1 = mk1.lat * (Math.PI/180); // Convert degrees to radians
+        let rlat2 = mk2.lat * (Math.PI/180); // Convert degrees to radians
+        let difflat = rlat2-rlat1; // Radian difference (latitudes)
+        let difflon = (mk2.lng-mk1.lng) * (Math.PI/180); // Radian difference (longitudes)
         
-        var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+        let d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
         return d;
         };
 
