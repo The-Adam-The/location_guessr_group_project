23cc081685610841fa795790a076dd6b40d2e70e
The-Adam-The <adamwhite930@gmail.com>
2022-03-14 12:41:16 +0000

    added round incrementer to GameContainer.js and added nextRound() to CheckButton.js

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -81,7 +81,7 @@ const {isLoaded, loadError} = useLoadScript({
                 <Question question={question}/>
                 <Map question={question} checkButton={checkButton} setCheckButton={setCheckButton} markers={markers} setMarkers={setMarkers} center={center} setCenter={setCenter} onMapLoad={onMapLoad}/>
             </div>
-            <CheckButton markers={markers} setMarkers={setMarkers} checkButton={checkButton} setCheckButton={setCheckButton} question={question} setCenter={setCenter} haversineDistance={haversineDistance} mapRef={mapRef} nextQuestion={nextQuestion}/>
+            <CheckButton nextRound={nextRound} markers={markers} setMarkers={setMarkers} checkButton={checkButton} setCheckButton={setCheckButton} question={question} setCenter={setCenter} haversineDistance={haversineDistance} mapRef={mapRef} nextQuestion={nextQuestion}/>
 
             <button id="rules-btn" onClick={() => setRulePopup(true)}>Rules</button>
             <RulesPopup trigger={rulePopup} setTrigger={setRulePopup}>
