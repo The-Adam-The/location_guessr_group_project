1765917b06f8db18793ef508beca0dc1e3465fd1
The-Adam-The <adamwhite930@gmail.com>
2022-03-14 14:36:19 +0000

    added more questions to the seeds.js

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -13,8 +13,61 @@ db.questions.insertMany(
                 lng: -90.02262318299292
             },
         },
-        imageLink: "https://en.wikipedia.org/wiki/Graceland#/media/File:Graceland_Memphis_Tennessee.jpg"
+        imageLink: "https://upload.wikimedia.org/wikipedia/commons/5/54/Graceland_Memphis_Tennessee.jpg"
     },
+    { 
+        name: "Beatles",
+        question: "The Beatles are famous for coming from what city?",
+        location: {
+            place: "Liverpool",
+            country: "United Kingdom",
+            coords: {
+                lat: 53.40518831136327,
+                lng: -2.9810293746230516
+            },
+        },
+        imageLink: "https://upload.wikimedia.org/wikipedia/commons/d/df/The_Fabs.JPG"
+    },
+    { 
+        name: "Taj Mahal",
+        question: "Where is the Taj Mahal?",
+        location: {
+            place: "Agra, Uttar Pradesh",
+            country: "India",
+            coords: {
+                lat: 27.17535476245167, 
+                lng: 78.04211001154499
+            },
+        },
+        imageLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1280px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg"
+    },
+    { 
+        name: "Lake Ellsworth",
+        question: "Where is Lake Ellsworth? (Not USA)",
+        location: {
+            place: "West Antarctica",
+            country: "Antarctica",
+            coords: {
+                lat: -78.95302926451168, 
+                lng: -91.04380789078864
+            },
+        },
+        imageLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/2007_mather-lake_hg.jpg/1280px-2007_mather-lake_hg.jpg"
+    },
+    {
+        name: "Tsar Bomb",
+        question: "Where did the USSR drop the Tsar bomba in  1961?",
+        location: {
+            place: "Severny Island, Arkhangelsk Oblast",
+            country: "Russia",
+            coords: {
+                lat: 73.807222, 
+                lng: 54.981667
+            },
+        },
+        imageLink: "https://upload.wikimedia.org/wikipedia/en/c/c9/Tsar_photo11.jpg"
+    },
+
     {
         name: "Uluru",
         question: "Uluru is located where?",
