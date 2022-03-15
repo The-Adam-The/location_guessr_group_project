e88f6eb68c38fa9013891be1b47c72ea01c661c8
Rachel McDonald <rach-mcdonald@outlook.com>
2022-03-15 12:25:52 +0000

    logo added to the homepage

--- client/src/containers/LogInContainer.js
+++ client/src/containers/LogInContainer.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header';
 import LogInForm from '../components/LogInForm'
 import Instructions from '../components/Instructions'
+import logo from '../images/globe_with_background.png';
 
 const LogInContainer = ({displayGamePage, setUserName}) => {
     return(
@@ -8,7 +9,7 @@ const LogInContainer = ({displayGamePage, setUserName}) => {
             <Header />
             <h3>How many locations can you pin on the map?</h3>
             <h5>Test your trivia and geography knowledge in one game</h5>
-            <img src="https://placebear.com/700/400" alt="bears picture" />
+            <img src={logo} alt="bears picture" />
             <h5 className="info-box">3 Questions per round</h5>
             <h5 className="info-box">Game Time: Unlimited</h5>
             <Instructions/>
