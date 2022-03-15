7361768a1a6a39625a300c39a5d9b6310edb6579
Rachel McDonald <rach-mcdonald@outlook.com>
2022-03-15 12:50:39 +0000

    article and section tags added to the LogInContainer.js file to target the headers and images within the new CSS file. Elements on the homepage centered.

--- client/src/containers/LogInContainer.js
+++ client/src/containers/LogInContainer.js
@@ -2,17 +2,24 @@ import Header from '../components/Header';
 import LogInForm from '../components/LogInForm'
 import Instructions from '../components/Instructions'
 import logo from '../images/globe_with_background.png';
+import './LogInContainer.css';
 
 const LogInContainer = ({displayGamePage, setUserName}) => {
     return(
         <div className="login-container">
             <Header />
-            <h3>How many locations can you pin on the map?</h3>
-            <h5>Test your trivia and geography knowledge in one game</h5>
-            <img src={logo} alt="bears picture" />
-            <h5 className="info-box">3 Questions per round</h5>
-            <h5 className="info-box">Game Time: Unlimited</h5>
-            <Instructions/>
+                <article className="page-info">
+                    <section className="headings">
+                        <h1>How many locations can you pin on the map?</h1>
+                        <h2>Test your trivia and geography knowledge in one game</h2>
+                    </section>
+                        <img className="globe" src={logo} alt="bears picture" />
+                        <section className="game-info">
+                            <h5 className="info-box">3 Questions per round</h5>
+                            <h5 className="info-box">Game Time: Unlimited</h5>
+                            <Instructions/>
+                        </section>
+                </article>
             <LogInForm displayGamePage={displayGamePage} setUserName={setUserName}/>
         </div>
     );
