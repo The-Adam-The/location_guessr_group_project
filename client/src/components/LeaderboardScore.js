e03c8f4178acdbd3a13d7345b4edcea0cdbb4a8c
James <jamesg487@gmail.com>
2022-03-15 15:18:14 +0000

    fixed bug where 0 accuracy value doent get updated causing question to not load

--- client/src/components/LeaderboardScore.js
+++ client/src/components/LeaderboardScore.js
@@ -3,8 +3,8 @@ const LeaderboardScore = ({score}) => {
         <li>
             <p>{score.name}:</p>
             <p>Points: {score.total.points}</p>
-            <p>Average Distance: {score.total.averageDistance}</p>
-            <p>Average Accuracy: {score.total.averageAccuracy}</p>
+            <p>Average Distance: {score.total.averageDistance}km</p>
+            <p>Average Accuracy: {score.total.averageAccuracy}%</p>
 
         </li>
     );
