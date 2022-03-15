1bbf24ee9ea51fbce8c9468c1570aeedf33a7b9c
Rachel McDonald <rach-mcdonald@outlook.com>
2022-03-15 22:04:51 +0000

    added icon to instructions button

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -1,7 +1,10 @@
 const Instructions = () => {
     return(
         <>
-            <button className="instructions-button">Instructions</button>
+            <button className="instructions-button">
+                Instructions 
+                <span className="toggle-id"><i class="fa fa-external-link" aria-hidden="true"></i></span>
+                </button>
         </>
     );
 };
