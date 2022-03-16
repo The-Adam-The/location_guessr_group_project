deba5ae1c02042b32e7b6e92efb4cfe6aecf9d96
hvirfvel <carolinehvirfvel@hotmail.com>
2022-03-16 11:41:59 +0000

    moved rules popup next to button

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -153,13 +153,14 @@ const GameContainer = ({displayScoresPage, userName, userScores, setUserScores, 
                         <Map question={question} checkButton={checkButton} setCheckButton={setCheckButton} markers={markers} setMarkers={setMarkers} center={center} setCenter={setCenter} onMapLoad={onMapLoad}/>
                         {checkButton ? <FeedbackOverlay checkButton={checkButton} indDistance={indDistance} indAccuracy={indAccuracy} question={question} />: null}
                     </div>
-                    <RulesPopup trigger={rulePopup} setTrigger={setRulePopup}>
-                        <h3>Game Rules:</h3>
-                        <br />
-                        <p>Drop your pin on the map when you have guessed the location from the clues!</p>
-                    </RulesPopup>
                     <nav className="question-buttons">
-                        <button className="rules-button" id="rules-btn" onClick={() => setRulePopup(true)}><span>?</span></button>
+                        <div className="rules-section">
+                            <button className="rules-button" id="rules-btn" onClick={() => setRulePopup(true)}><span>?</span></button>
+                            <RulesPopup className="rules-popup" trigger={rulePopup} setTrigger={setRulePopup}>
+                                <h3>Game Rules:</h3>
+                                <p>Drop your pin on the map when you have guessed the location from the clues!</p>
+                            </RulesPopup>
+                        </div>
                         <CheckButton className="check-button" setIndAccuracy={setIndAccuracy} roundNumber={roundNumber} displayScoresPage={displayScoresPage} nextRound={nextRound} markers={markers} setMarkers={setMarkers} checkButton={checkButton} setCheckButton={setCheckButton} question={question} setCenter={setCenter} mapRef={mapRef} postUserScores={postUserScores}/>
                     </nav>
                 </section>
